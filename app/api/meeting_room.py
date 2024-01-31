d9afe080afb0156b82c936826da1aafd915113aa
Yuri Tischenko <yuri.tishchenko@gmail.com>
2024-01-31 13:27:28 +0300

    ff

--- app/api/meeting_room.py
+++ app/api/meeting_room.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException 
+from fastapi import APIRouter, Depends, HTTPException
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.db import get_async_session
 from app.crud.meeting_room import create_meeting_room, get_room_id_by_name
 from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB
-
 
 router = APIRouter()
 
@@ -14,12 +16,13 @@ router = APIRouter()
 )
 async def create_new_meeting_room(
         meeting_room: MeetingRoomCreate,
+        session: AsyncSession = Depends(get_async_session),
 ):
-    room_id = await get_room_id_by_name(meeting_room.name)
+    room_id = await get_room_id_by_name(meeting_room.name, session)
     if room_id is not None:
         raise HTTPException(
             status_code=422,
             detail='Переговорка с таким именем уже существует!',
         )
-    new_room = await create_meeting_room(meeting_room)
-    return new_room 
+    new_room = await create_meeting_room(meeting_room, session)
+    return new_room
